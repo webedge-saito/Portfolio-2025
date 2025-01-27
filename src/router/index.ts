aa8f0d16b410c502e7ea2bc2c920e465037b8971
webedge-saito <saito-eri@webedge.jp>
2025-01-28 02:15:21 +0900

    errorFix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import FrontEndView from '../views/frontEndView.vue'
-import CodingView from '../views/codingView.vue'
+import FrontEndView from '../views/FrontEndView.vue'
+import CodingView from '../views/CodingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
